1fd9e87f0224e6e221ab78c48faaeb63a4f3c1e1
David Wilbanks <dwilbank@gmail.com>
2015-11-06 19:02:18 -0800

    add routes for layout, navbar

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #faq" do
+    it "returns http success" do
+      get :faq
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #pricing" do
+    it "returns http success" do
+      get :pricing
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #features" do
+    it "returns http success" do
+      get :features
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
